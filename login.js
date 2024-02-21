15cda7c42aaa745ed18417565047549b3b0cacbe
Mostafa <138389389+Mostafa7i@users.noreply.github.com>
2024-02-21 13:09:21 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,34 @@
+let userName = document.querySelector("#userName");
+let passward = document.querySelector("#passward");
+
+let loginSubmit = document.querySelector("#sign-in");
+
+localStorage.setItem("userName" , "mostafa")
+localStorage.setItem("password" , "123")
+getUser = localStorage.getItem("userName");
+getpassward = localStorage.getItem("password");
+
+loginSubmit.addEventListener("click" , function(e){
+  e.preventDefault()
+  if(userName.value ==="" || passward.value === ""){
+    alert("please enter data")
+  }else{
+    if(getUser === userName.value.trim() && getpassward === passward.value){
+      setTimeout(()=>{
+        window.location = "index.html";
+      },1500)
+    }else{
+      alert("sure from data")
+    }
+  }
+})
+
+
+function togglePasswordVisibility() {
+  if (passward.type === "password") {
+    passward.type = "text";
+  } else {
+    passward.type = "password";
+  }
+}
+
