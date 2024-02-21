b34968b7561663cf97234f0fbced8c2724a9741d
Mostafa <138389389+Mostafa7i@users.noreply.github.com>
2024-02-21 14:35:39 +0200

    Update login.js

--- login.js
+++ login.js
@@ -3,26 +3,40 @@ let passward = document.querySelector("#passward");
 
 let loginSubmit = document.querySelector("#sign-in");
 
-localStorage.setItem("userName" , "mostafa")
-localStorage.setItem("password" , "123")
+localStorage.setItem("userName", "mostafa");
+localStorage.setItem("password", "123");
 getUser = localStorage.getItem("userName");
 getpassward = localStorage.getItem("password");
 
-loginSubmit.addEventListener("click" , function(e){
-  e.preventDefault()
-  if(userName.value ==="" || passward.value === ""){
-    alert("please enter data")
-  }else{
-    if(getUser === userName.value.trim() && getpassward === passward.value){
-      setTimeout(()=>{
+loginSubmit.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (userName.value === "" || passward.value === "") {
+    Swal.fire({
+      title: "Warning!",
+      text: "Please Fill Data",
+      icon: "warning",
+    });
+  } else {
+    if (getUser === userName.value.trim() && getpassward === passward.value) {
+      setTimeout(() => {
         window.location = "index2.html";
-      },1500)
-    }else{
-      alert("User name or password is wrong")
+      }, 1500);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "User Name or Password is wrong!",
+      });
     }
   }
-})
-
+});
 
 function togglePasswordVisibility() {
   if (passward.type === "password") {
@@ -31,4 +45,3 @@ function togglePasswordVisibility() {
     passward.type = "password";
   }
 }
-
