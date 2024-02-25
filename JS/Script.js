e3bd05e99df49f4a0d208d96548c17026b0b8df9
Mostafa <138389389+Mostafa7i@users.noreply.github.com>
2024-02-25 08:09:33 +0200

    Update Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -1,9 +1,33 @@
+
+// ////////////////////////////////////////////////////////////////////////////////////////
+let userinfo = document.querySelector("#user_info")
+let userData = document.querySelector("#user")
+let links = document.querySelector("#links")
+
+let logoutBtn = document.querySelector("#logout")
+
+
+if(localStorage.getItem("username")){
+    // links.remove()
+    userData.style.color ="yollow"
+    userData.style.fontWeight = "900"
+    userData.innerHTML = localStorage.getItem("username")
+    userData.style.textTransform = "capitalize"
+}
+// logoutBtn.addEventListener("click" , function(){
+//     localStorage.clear()
+//     setTimeout(()=>{
+//         // window.location = "register.html"
+//     } , 1000)
+// })
+
+
 let sr = document.querySelector("#btn");
 
 sr.addEventListener("click", add);
 
 function add() {
-  alert("لازال قيد التحديث");
+  // alert("لازال قيد التحديث");
 }
 
 let close = document.querySelector("#close");
@@ -13,4 +37,8 @@ close.addEventListener("click", () => {
   btnf.style.display = "none";
 });
 
-
+let gets = document.getElementById('get')
+gets.addEventListener('click' , get)
+function get(){
+window.location = "index3.html"
+}
