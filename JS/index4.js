683f861dad19c25b1a4c261693584a3e720703b2
Mostafa <138389389+Websits-M@users.noreply.github.com>
2024-03-28 03:50:54 +0200

    Update index4.js
    Signed-off-by: Mostafa <138389389+Websits-M@users.noreply.github.com>

--- JS/index4.js
+++ JS/index4.js
@@ -2,6 +2,19 @@ let check = document.querySelector("#check1");
 let hide = document.querySelector("#hid");
 
 
+$(document).ready(function(){
+    /* by default hide all radio_content div elements except first element */
+    $(".content .radio_content").hide();
+    $(".content .radio_content:first-child").show();
+
+    /* when any radio element is clicked, Get the attribute value of that clicked radio element and show the radio_content div element which matches the attribute value and hide the remaining tab content div elements */
+    $(".radio_wrap").click(function(){
+      var current_raido = $(this).attr("data-radio");
+      $(".content .radio_content").hide();
+      $("."+current_raido).show();
+    })
+  });
+
 function showandhid(){
     hide.style.display = check.checked ? "block" : "none"
 }
