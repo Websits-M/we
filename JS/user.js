a846e21414c2f15659df5ab745cbf7a0eae4ba37
Mostafa <138389389+Websits-M@users.noreply.github.com>
2024-03-28 10:39:22 +0200

    Update user.js
    Signed-off-by: Mostafa <138389389+Websits-M@users.noreply.github.com>

--- JS/user.js
+++ JS/user.js
@@ -8,6 +8,7 @@ let logoutBtn = document.querySelector("#logout")
 
 function log(){
   if(localStorage.getItem("username") == ("mostafa")){
+    window.location = "index.html"
    localStorage.clear()
   }
 }
